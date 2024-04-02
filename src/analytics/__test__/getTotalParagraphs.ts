7e3fd5c38b7b3f829fc810300156ffc7d2145cb8
Clara <clarafdezfraga@gmail.com>
2024-04-03 00:58:31 +0200

    Add more test to getTotalParagraphs

--- src/analytics/__test__/getTotalParagraphs.ts
+++ src/analytics/__test__/getTotalParagraphs.ts
@@ -1,9 +1,26 @@
-import { getTotalParagraphs } from "../index";
+import { getTotalParagraphs } from "../index.js";
 
-const testingText = "Este es un texto de prueba \n Que incluye 2 parrafos";
-console.log(testingText);
-console.log(getTotalParagraphs(testingText));
-if (getTotalParagraphs(testingText) === 2) {
+const emptyText = "";
+
+console.log("getTotalParagraphs should return 0 if text is empty.");
+if (getTotalParagraphs(emptyText) === 0) {
+  console.log("Test passed");
+} else {
+  console.log("Test failed");
+}
+
+const oneParagraphText = "Este es un texto de 1 párrafo";
+console.log("getTotalParagraphs should return 1 if text has 1 paragraph.");
+if (getTotalParagraphs(oneParagraphText) === 1) {
+  console.log("Test passed");
+} else {
+  console.log("Test failed");
+}
+
+const twoParagraphsText =
+  "Este es un texto de prueba \n Que incluye 2 parrafos";
+console.log("getTotalParagraphs should return 2 if text has 2 paragraphs.");
+if (getTotalParagraphs(twoParagraphsText) === 2) {
   console.log("Test passed");
 } else {
   console.log("Test failed");
--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -1,7 +1,19 @@
+const isEmptyText = (text: string): boolean => {
+  return text === "";
+};
+
 export const getTotalParagraphs = (text: string): number => {
-  if (text === "") {
+  if (isEmptyText(text)) {
     return 0;
   } else {
     return text.split("\n").length;
+  }
+};
+
+export const getTotalWords = (text: string): number => {
+  if (isEmptyText(text)) {
+    return 0;
+  } else {
+    return text.split(" ").length;
   }
 };
