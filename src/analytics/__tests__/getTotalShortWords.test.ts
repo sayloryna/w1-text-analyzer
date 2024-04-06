bc3e11e3f9d60a0e6a3612eefdcda1879445f234
Clara <clarafdezfraga@gmail.com>
2024-04-06 17:14:08 +0200

    Add shortWords couter tests

---
+++ src/analytics/__tests__/getTotalShortWords.test.ts
@@ -0,0 +1,30 @@
+import { getTotalShortWords } from "../index.js";
+
+const emptyText = "";
+
+console.log(`
+If the  text is empty it should retunr 0:`);
+
+const areTotalShortWordsZero = getTotalShortWords(emptyText) === 0;
+
+if (areTotalShortWordsZero) {
+  console.log("Test passed");
+} else {
+  console.log("Test Failed");
+}
+
+console.log(
+  `
+  If the text is not empty it should return the total of short words(less than 4 letter or numbers)`
+);
+
+const fiveShourtWordsText = `en este texto, hay: 
+5 palabras mega! cortas`;
+
+const areTotalShortWordsFive = getTotalShortWords(fiveShourtWordsText) === 5;
+
+if (areTotalShortWordsFive) {
+  console.log("Test passed");
+} else {
+  console.log("Test Failed");
+}
