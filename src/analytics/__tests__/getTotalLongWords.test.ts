5c04dada087ab5557b3ba6e18ca17233e2fb9927
Clara <clarafdezfraga@gmail.com>
2024-04-06 17:44:01 +0200

    Add longWords counter

---
+++ src/analytics/__tests__/getTotalLongWords.test.ts
@@ -0,0 +1,30 @@
+import { getTotalLongWords } from "../index.js";
+
+const emptyText = "";
+
+console.log(`
+If the  text is empty it should retunr 0:`);
+
+const areTotalLongtWordsZero = getTotalLongWords(emptyText) === 0;
+
+if (areTotalLongtWordsZero) {
+  console.log("Test passed");
+} else {
+  console.log("Test Failed");
+}
+
+console.log(
+  `
+If the text is not empty it should return the total of short words(less than 4 letter or numbers)`
+);
+
+const fiveLongtWordsText = `en este texto, hay: 
+5 palabra mega ¡largas! Electroencefalografista, Esternocleidomastoideo, Inconstitucionalidad Otorrinolaringológicamente ,  y Desoxirribonucleico `;
+
+const areTotalShortWordsFive = getTotalLongWords(fiveLongtWordsText) === 5;
+
+if (areTotalShortWordsFive) {
+  console.log("Test passed");
+} else {
+  console.log("Test Failed");
+}
